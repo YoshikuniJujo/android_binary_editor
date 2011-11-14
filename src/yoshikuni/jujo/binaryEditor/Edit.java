086e124c861d52296a6578f94423587702eb4648
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-11-14 18:03:52 +0900

    clear Edit class

--- src/yoshikuni/jujo/binaryEditor/Edit.java
+++ src/yoshikuni/jujo/binaryEditor/Edit.java
@@ -5,13 +5,8 @@ import java.util.*;
 
 class Edit
 {
-	private String str = "";
-	private String buf = "";
-	private int utfBuf = 0;
-	private int num = -1;
-	private int bytes = 0;
-	private int cursor = 0;
-	private String path = "";
+
+	String str;
 
 	public static void main(String[] args) throws IOException
 	{
@@ -52,185 +47,21 @@ class Edit
 		System.out.println(testEdit.get());
 	}
 
-	public String get() {
-		String ret = str.substring(0, cursor);
-		if (utfBuf > 0) ret += "~" + utfBuf;
-		if (num > -1) ret += "^" + num;
-		ret += "_";
-		ret += str.substring(cursor, str.length());
-		return ret;
+	public String get()
+	{
+		return str;
 	}
 
-	public void push(int n) {
-		if (n == -1) {
-			if (num > -1) {
-				num = -1;
-			} else if (utfBuf > 0) {
-				utfBuf = 0; bytes = 0;
-			} else {
-				if (!str.equals("") && cursor > 0) {
-					str = str.substring(0, cursor - 1)
-					+ str.substring(cursor, str.length());
-					cursor--;
-				}
-			}
-		} else if (n == -2) {
-			if (num == -1 && utfBuf == 0 && cursor > 0) {
-				cursor--;
-			}
-		} else if (n == -3) {
-			if (num == -1 && utfBuf == 0 && cursor < str.length()) {
-				cursor++;
-			}
-		} else if (n == -4) {
-			if (num == -1 && utfBuf == 0 && cursor > 0) {
-				cursorUp();
-			}
-		} else if (n == -5) {
-			if (num == -1 && utfBuf == 0 && cursor < str.length()) {
-				cursorDown();
-			}
-		} else if (n == -6) {
-			copy();
-		} else if (n == -7) {
-			paste();
-		} else if (num < 0) {
-			num = n;
-		} else {
-			add(num << 4 | n);
-			num = -1;
-		}
+	public void push(int n)
+	{
 	}
 
 	public void setPath(String p) throws IOException
 	{
-		if (path != p) {
-			cursor = 0;
-			utfBuf = 0;
-			num = -1;
-			bytes = 0;
-			str = "";
-			BufferedReader reader
-				= new BufferedReader(new FileReader(p));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				str += line + "\n";
-			}
-			reader.close();
-			path = p;
-		}
 	}
 
 	public void save() throws IOException
 	{
-		PrintWriter writer =
-			new PrintWriter(new BufferedWriter(new FileWriter(path)));
-		writer.print(str);
-		writer.close();
 	}
 
-	private void add(int c) {
-		if (bytes > 0) {
-			utfBuf = (utfBuf << 6) | (c & ~(1 << 7));
-			bytes--;
-			if (bytes < 1) {
-				str = str.substring(0, cursor) + (char)utfBuf
-					+ str.substring(cursor, str.length());
-				utfBuf = 0;
-				cursor++;
-			}
-		} else {
-			int n = 0;
-			utfBuf = c;
-			for (int i = 7; i > 1; i--) {
-				if ((c & 1 << i) == 0) break;
-				utfBuf &= ~(1 << i);
-				n++;
-			}
-			bytes = n - 1;
-			if (n == 0) {
-				utfBuf = 0; // str += (char)c;
-				str = str.substring(0, cursor) + (char)c
-					+ str.substring(cursor, str.length());
-				cursor++;
-			}
-		}
-	}
-
-	private void cursorUp()
-	{
-		cursor = preIndex(str, '\n', cursor);
-	}
-
-	private void cursorDown()
-	{
-		cursor = postIndex2(str, '\n', cursor) + 1;
-	}
-
-	private void copy()
-	{
-		buf = str.substring(preIndex2(str, '\n', cursor) + 1,
-			postIndex2(str, '\n', cursor) + 1);
-	}
-
-	private void paste()
-	{
-		int next = postIndex2(str, '\n', cursor) + 1;
-		str = str.substring(0, next) + buf
-			+ str.substring(next, str.length());
-	}
-
-	private static int preIndex2(String str, char c, int here)
-	{
-		Integer ret = -1;
-		LinkedList<Integer> ids = indices(str, c);
-		for (Iterator i = ids.iterator(); i.hasNext();) {
-			Integer tmp = (Integer)i.next();
-			if (tmp >= here) break;
-			ret = tmp;
-		}
-		return ret;
-	}
-
-	private static int preIndex(String str, char c, int here)
-	{
-		Integer ret = 0;
-		LinkedList<Integer> ids = indices(str, c);
-		for (Iterator i = ids.iterator(); i.hasNext();) {
-			Integer tmp = (Integer)i.next();
-			if (tmp >= here) break;
-			ret = tmp;
-		}
-		return ret;
-	}
-
-	private static int postIndex2(String str, char c, int here)
-	{
-		Integer ret = 0;
-		LinkedList<Integer> ids = indices(str, c);
-		for (Iterator i = ids.iterator(); i.hasNext();) {
-			ret = (Integer)i.next();
-			if (ret >= here) break;
-		}
-		return ret;
-	}
-
-	private static int postIndex(String str, char c, int here)
-	{
-		Integer ret = 0;
-		LinkedList<Integer> ids = indices(str, c);
-		for (Iterator i = ids.iterator(); i.hasNext();) {
-			ret = (Integer)i.next();
-			if (ret > here) break;
-		}
-		return ret;
-	}
-
-	private static LinkedList<Integer> indices(String str, char c)
-	{
-		LinkedList<Integer> ret = new LinkedList<Integer>();
-		for (int i = 0; i < str.length(); i++)
-			if (str.charAt(i) == c) ret.add(i);
-		return ret;
-	}
 }
