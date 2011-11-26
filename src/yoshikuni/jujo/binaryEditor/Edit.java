03a4206850d8fe4a813eaa234d376742ddf515d2
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2011-11-26 12:39:37 +0900

    change from binEdit

--- src/yoshikuni/jujo/binaryEditor/Edit.java
+++ src/yoshikuni/jujo/binaryEditor/Edit.java
@@ -6,7 +6,10 @@ import java.util.*;
 class Edit
 {
 
-	String str;
+	LinkedList<Byte> contents = new LinkedList<Byte>();
+	int cursor = 0;
+	int high = -1;
+	String path;
 
 	public static void main(String[] args) throws IOException
 	{
@@ -49,19 +52,69 @@ class Edit
 
 	public String get()
 	{
-		return str;
+		String ret = "";
+		int i;
+		for (i = 0; i < contents.size(); i++) {
+			if (i == cursor) {
+				if (high > -1) ret += String.format("%x", high);
+				ret += "_";
+			}
+			ret += String.format("%02x ", contents.get(i));
+			if (i % 16 == 15) ret += "\n";
+		}
+		if (i == cursor) {
+			if (high > -1) ret += String.format("%x", high);
+			ret += "_";
+		}
+		return ret;
 	}
 
 	public void push(int n)
 	{
+		if (n > -1) {
+			if (high > -1) {
+				contents.add(cursor, (byte)(high << 4 | n));
+				high = -1;
+				cursor++;
+			} else {
+				high = n;
+			}
+		} else if (n == -1) {
+			if (cursor > 0) {
+				cursor--;
+				contents.remove(cursor);
+			}
+		} else if (n == -2) {
+			if (cursor > 0) cursor--;
+		} else if (n == -3) {
+			if (cursor < contents.size()) cursor++;
+		} else if (n == -4) {
+			if (cursor > 15) cursor -= 16;
+		} else if (n == -5) {
+			if (cursor < contents.size() - 15) cursor += 16;
+		}
 	}
 
 	public void setPath(String p) throws IOException
 	{
+		path = p;
+		BufferedInputStream bis = new BufferedInputStream(
+			new FileInputStream(p));
+		int b;
+		while ((b = bis.read()) > -1) {
+			contents.add((byte)b);
+		}
+		bis.close();
 	}
 
 	public void save() throws IOException
 	{
+		BufferedOutputStream bos = new BufferedOutputStream(
+			new FileOutputStream(path));
+		for (int i = 0; i < contents.size(); i++) {
+			bos.write(contents.get(i));
+		}
+		bos.close();
 	}
 
 }
